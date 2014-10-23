58255fdeba8a9cef7317f3a550fa8dc463435fc1
CJ Carey <perimosocordiae@gmail.com>
2014-10-23 17:36:44 -0400

    Adding some tests

--- graphs/generators/tests/test_swiss_roll.py
+++ graphs/generators/tests/test_swiss_roll.py
@@ -12,8 +12,15 @@ class TestSwissRoll(unittest.TestCase):
 
   def test_swiss_roll(self):
     X = sr.swiss_roll(6, 10)
+    self.assertEqual(X.shape, (10, 3))
+    X, theta = sr.swiss_roll(3.0, 25, theta_noise=0, radius_noise=0,
+    	                     return_theta=True)
+    self.assertEqual(X.shape, (25, 3))
+    self.assertEqual(theta.shape, (25,))
+    self.assertAlmostEqual(theta.max(), 3.0)
 
   def test_error_ratio(self):
+  	# TODO: test
     sr.error_ratio
 
 
