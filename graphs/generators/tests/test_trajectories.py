63590751d3421a44617e299bf5e9899a47b4bde6
CJ Carey <perimosocordiae@gmail.com>
2014-10-23 18:22:04 -0400

    Adding tests

--- graphs/generators/tests/test_trajectories.py
+++ graphs/generators/tests/test_trajectories.py
@@ -5,17 +5,38 @@ import numpy as np
 import unittest
 from numpy.testing import assert_array_equal
 
-from graphs.generators import trajectories as traj
+from graphs.generators import shapes, trajectories as traj
 
 
 class TestTrajectories(unittest.TestCase):
 
   def test_concat_trajectories(self):
-    traj.concat_trajectories
+    expected = [[0,1,0,0,0],[1,0,0,0,0],[0,0,0,1,0],[0,0,1,0,1],[0,0,0,1,0]]
+    G = traj.concat_trajectories([2, 3], directed=False)
+    assert_array_equal(G.matrix(dense=True), expected)
 
   def test_chunk_up(self):
-    traj.chunk_up
-
+    T = shapes.SCurve().trajectories(2, 4)
+    expected = [[0,1,0,0,0,0,0,0],
+                [1,0,1,0,0,0,0,0],
+                [0,1,0,1,0,0,0,0],
+                [0,0,1,0,0,0,0,0],
+                [0,0,0,0,0,1,0,0],
+                [0,0,0,0,1,0,1,0],
+                [0,0,0,0,0,1,0,1],
+                [0,0,0,0,0,0,1,0]]
+    G = traj.chunk_up(T, directed=False)
+    assert_array_equal(G.matrix(dense=True), expected)
+    expected = [[0,1,0,0,0,0,0,0],
+                [1,0,0,0,0,0,0,0],
+                [0,0,0,1,0,0,0,0],
+                [0,0,1,0,0,0,0,0],
+                [0,0,0,0,0,1,0,0],
+                [0,0,0,0,1,0,0,0],
+                [0,0,0,0,0,0,0,1],
+                [0,0,0,0,0,0,1,0]]
+    G = traj.chunk_up(T, chunk_size=2, directed=False)
+    assert_array_equal(G.matrix(dense=True), expected)
 
 if __name__ == '__main__':
   unittest.main()
