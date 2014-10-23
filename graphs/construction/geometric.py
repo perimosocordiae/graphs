db935bcb75c79e662fa18e32300fb350bb8824ed
CJ Carey <perimosocordiae@gmail.com>
2014-10-23 17:36:28 -0400

    Fixing gabriel graph, adding tests

--- graphs/construction/geometric.py
+++ graphs/construction/geometric.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.spatial import Delaunay
 from scipy.sparse import coo_matrix
 from sklearn.metrics.pairwise import pairwise_distances, paired_distances
-from graphs import Graph, plot_graph
+from graphs import Graph
 
 __all__ = ['delaunay_graph', 'gabriel_graph', 'relative_neighborhood_graph']
 
@@ -28,12 +28,13 @@ def delaunay_graph(X):
 
 def gabriel_graph(X, metric='euclidean'):
   a,b = np.triu_indices(X.shape[0], k=1)
-  midpoints = (X[a] - X[b]) / 2
-  Dmid = pairwise_distances(midpoints, X, metric=metric)
+  midpoints = (X[a] + X[b]) / 2
+  Dmid = pairwise_distances(midpoints, X, metric=metric).min(axis=1)
   Dedge = paired_distances(X[a], X[b], metric=metric)
-  mask = np.all(Dedge[:,None] <= Dmid, axis=1)
+  mask = (Dedge - Dmid * 2) < 1e-10
   pairs = np.transpose((a[mask],b[mask]))
-  return Graph.from_edge_pairs(np.vstack((pairs,pairs[:,::-1])))
+  pairs = np.vstack((pairs,pairs[:,::-1]))
+  return Graph.from_edge_pairs(pairs, X.shape[0])
 
 
 def relative_neighborhood_graph(X, metric='euclidean'):
@@ -43,14 +44,5 @@ def relative_neighborhood_graph(X, metric='euclidean'):
   Dedge = paired_distances(X[a], X[b], metric=metric)
   mask = np.all((Dedge[:,None] <= Da) | (Dedge[:,None] <= Db), axis=1)
   pairs = np.transpose((a[mask],b[mask]))
-  return Graph.from_edge_pairs(np.vstack((pairs,pairs[:,::-1])))
-
-
-if __name__ == '__main__':
-  from matplotlib.pyplot import subplots
-  X = np.random.random((5,2))
-  _, axes = subplots(ncols=3)
-  plot_graph(X, delaunay_graph(X), title='Delaunay', ax=axes[0])
-  plot_graph(X, gabriel_graph(X), title='Gabriel', ax=axes[1])
-  plot_graph(X, relative_neighborhood_graph(X),
-             title='Relative Neighborhood', ax=axes[2])()
+  pairs = np.vstack((pairs,pairs[:,::-1]))
+  return Graph.from_edge_pairs(pairs, X.shape[0])
--- graphs/construction/tests/test_geometric.py
+++ graphs/construction/tests/test_geometric.py
@@ -10,15 +10,23 @@ from graphs.construction import (
 
 
 class TestGeometric(unittest.TestCase):
+  def setUp(self):
+    self.pts = np.array([[0,0],[1.1,2],[3.3,2],[-0.9,0],[0.5,1]])
 
   def test_delaunay(self):
-    pts = np.array([[0,0],[1,2],[3,2],[-1,0]])
+    expected = [[0,0,1,1,1],[0,0,1,1,1],[1,1,0,0,1],[1,1,0,0,1],[1,1,1,1,0]]
+    G = delaunay_graph(self.pts)
+    assert_array_equal(G.matrix(dense=True), expected)
 
   def test_gabriel(self):
-    pts = np.array([[0,0],[1,2],[3,2],[-1,0]])
+    expected = [[0,0,0,1,1],[0,0,1,0,1],[0,1,0,0,0],[1,0,0,0,0],[1,1,0,0,0]]
+    G = gabriel_graph(self.pts)
+    assert_array_equal(G.matrix(dense=True), expected)
 
   def test_relative_neighborhood(self):
-    pts = np.array([[0,0],[1,2],[3,2],[-1,0]])
+    expected = [[0,0,0,1,1],[0,0,1,0,1],[0,1,0,0,0],[1,0,0,0,0],[1,1,0,0,0]]
+    G = relative_neighborhood_graph(self.pts)
+    assert_array_equal(G.matrix(dense=True), expected)
 
 if __name__ == '__main__':
   unittest.main()
