7cef6ddd1bb7714ff062762feaeeca0b3649ea85
CJ Carey <perimosocordiae@gmail.com>
2014-10-24 00:33:37 -0400

    hacky test for directed graph construction

--- graphs/construction/directed.py
+++ graphs/construction/directed.py
@@ -1,32 +1,35 @@
 import numpy as np
-from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.metrics.pairwise import paired_distances
 from graphs import Graph, plot_graph
 from neighbors import neighbor_graph
 
 
-def jeff_graph(trajectories, k=5, verbose=False, pruning_thresh=0):
+def jeff_graph(trajectories, k=5, verbose=False, pruning_thresh=0, return_coords=False):
   '''Directed graph construction alg. from Johns & Mahadevan, ICML '07.
   trajectories: list of NxD arrays of ordered states
   '''
   X = np.vstack(trajectories)
-  W = neighbor_graph(X, k=k, symmetrize=False)
+  G = neighbor_graph(X, k=k, symmetrize=False)
   if pruning_thresh > 0:
-    jeff_prune_edges(X, W, map(len, trajectories), pruning_thresh,
-                     copy=False, verbose=verbose)
-  return W, X
+    traj_len = map(len, trajectories)
+    G = jeff_prune_edges(G, X, traj_len, pruning_thresh, verbose=verbose)
+  if return_coords:
+    return G, X
+  return G
 
 
-def jeff_prune_edges(X, G, traj_lengths, pruning_thresh=0.1, verbose=False):
+def jeff_prune_edges(G, X, traj_lengths, pruning_thresh=0.1, verbose=False):
   '''Prune edges in graph G via cosine distance with trajectory edges.'''
   W = G.matrix(dense=True).copy()
+  degree = G.degree(kind='out', unweighted=True)
   i = 0
   num_bad = 0
   for n in traj_lengths:
-    s,t = np.nonzero(W[i:i+n])
-    s = s + i
-    v = X[t] - X[s]
-    v_a = np.diff(X[i:i+n], axis=0)
-    theta = pairwise_distances(v, v_a, 'cosine')
+    s,t = np.nonzero(W[i:i+n-1])
+    graph_edges = X[t] - X[s+i]
+    traj_edges = np.diff(X[i:i+n], axis=0)
+    traj_edges = np.repeat(traj_edges, degree[i:i+n-1], axis=0)
+    theta = paired_distances(graph_edges, traj_edges, 'cosine')
     bad_edges = theta > pruning_thresh
     W[s[bad_edges],t[bad_edges]] = 0
     if verbose:
@@ -35,29 +38,3 @@ def jeff_prune_edges(X, G, traj_lengths, pruning_thresh=0.1, verbose=False):
   if verbose:
     print 'removed %d bad edges' % num_bad
   return Graph.from_adj_matrix(W)
-
-
-def demo_mcar(num_traj=100):
-  from matplotlib import pyplot
-  from downsample import downsample_trajectories
-  from graphs.generators.mountain_car import sample_mcar_trajectories
-
-  trajectories = sample_mcar_trajectories(num_traj, min_length=1)
-  print sum(map(len, trajectories)), 'total samples'
-
-  # downsample
-  trajectories = downsample_trajectories(trajectories, 0.0001)
-  print sum(map(len, trajectories)), 'after downsampling'
-
-  # build graphs
-  W_knn, X = jeff_graph(trajectories, k=5, verbose=True)
-  W_jeff = jeff_prune_edges(X, W_knn, map(len, trajectories),
-                            pruning_thresh=0.1, verbose=True)
-
-  # compare
-  _, (ax1, ax2) = pyplot.subplots(ncols=2)
-  plot_graph(W_knn, X, title='knn', ax=ax1)
-  plot_graph(W_jeff, X, title='Jeff', ax=ax2)()
-
-if __name__ == '__main__':
-  demo_mcar()
--- graphs/construction/tests/test_directed.py
+++ graphs/construction/tests/test_directed.py
@@ -5,13 +5,20 @@ import numpy as np
 import unittest
 from numpy.testing import assert_array_equal
 
+from graphs.generators.shapes import SCurve
 from graphs.construction import jeff_graph, jeff_prune_edges
 
 
 class TestDirected(unittest.TestCase):
 
   def test_jeff_graph(self):
-    pts = np.array([[0,0],[1,2],[3,2],[-1,0]])
+    np.random.seed(1234)
+    traj = SCurve().trajectories(5, 20)
+    G, X = jeff_graph(traj, k=5, pruning_thresh=0, return_coords=True)
+    P = jeff_graph(traj, k=5, pruning_thresh=0.1)
+    self.assertEqual(X.shape, (100, 3))
+    self.assertEqual(G.num_edges(), 500)
+    self.assertEqual(P.num_edges(), 419)
 
 
 if __name__ == '__main__':
