50b99e05bb3bfbc8a8b059b893c29bab5547b98f
CJ Carey <perimosocordiae@gmail.com>
2014-10-24 00:09:25 -0400

    Fixing and testing sparse regularized graph

--- graphs/construction/regularized.py
+++ graphs/construction/regularized.py
@@ -23,10 +23,11 @@ def sparse_regularized_graph(X, positive=False):
   n,d = X.shape
   I = np.eye(d)
   # Choose an efficient Lasso solver
+  cv = min(d, 3)
   if positive or d < n:
-    clf = linear_model.LassoCV(positive=positive)
+    clf = linear_model.LassoCV(positive=positive, cv=cv)
   else:
-    clf = linear_model.LassoLarsCV()
+    clf = linear_model.LassoLarsCV(cv=cv)
   # Normalize all samples
   X = X / np.linalg.norm(X, ord=2, axis=1)[:,None]
   # Solve for each row of W
@@ -43,29 +44,3 @@ def sparse_regularized_graph(X, positive=False):
     W[i,:i] = a[:i]
     W[i,i+1:] = a[i:]
   return Graph.from_adj_matrix(W)
-
-
-def demo():
-  from matplotlib.pyplot import subplots, set_cmap
-  from neighbors import neighbor_graph
-  from common.synthetic_data import gaussian_clusters
-
-  X = gaussian_clusters(5, 100, 20)
-  G_l1 = sparse_regularized_graph(X)
-  G_spg = sparse_regularized_graph(X, positive=True)
-  G_knn = neighbor_graph(X, k=4, weighting='none')
-
-  _, axes = subplots(nrows=2, ncols=3)
-  axes[1,0].imshow(G_l1.matrix(), interpolation='nearest')
-  axes[1,1].imshow(G_spg.matrix(), interpolation='nearest')
-  axes[1,2].imshow(G_knn.matrix(), interpolation='nearest')
-  set_cmap('YlOrRd')
-  for ax in axes[0]:
-    ax.set_axis_bgcolor('gray')
-  X2d = X[:,:2]
-  plot_graph(G_l1, X2d, ax=axes[0,0], title='L1', unweighted=False)
-  plot_graph(G_spg, X2d, ax=axes[0,1], title='SPG', unweighted=False)
-  plot_graph(G_knn, X2d, ax=axes[0,2], title='kNN', unweighted=False)()
-
-if __name__ == '__main__':
-  demo()
