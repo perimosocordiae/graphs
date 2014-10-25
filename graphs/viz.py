8c88c42d99530ef6d8653028a287ce64d90e528a
CJ Carey <perimosocordiae@gmail.com>
2014-10-25 16:48:03 -0400

    Add cmap kwarg
    Useful for plotting probabilistic-weighted edges

--- graphs/viz.py
+++ graphs/viz.py
@@ -8,13 +8,14 @@ __all__ = ['plot_graph']
 
 
 def plot_graph(G, coordinates, undirected=False, unweighted=False, fig=None,
-               ax=None, edge_style=None, vertex_style=None, title=None):
+               ax=None, edge_style=None, vertex_style=None, title=None,
+               cmap=None):
   X = np.atleast_2d(coordinates)
   assert X.shape[1] in (2,3), 'can only plot graph for 2d or 3d coordinates'
   is_3d = (X.shape[1] == 3)
   if ax is None:
     ax = _get_axis(is_3d, fig)
-  edge_kwargs = dict(colors='r', linestyles='-', zorder=1)
+  edge_kwargs = dict(colors='b', linestyles='-', zorder=1)
   vertex_kwargs = dict(marker='o', c='k', s=20, edgecolor='none', zorder=2)
   if edge_style:
     if isinstance(edge_style, basestring):
@@ -27,9 +28,9 @@ def plot_graph(G, coordinates, undirected=False, unweighted=False, fig=None,
   if not unweighted and G.is_weighted():
     edge_kwargs['array'] = G.edge_weights()
   if not undirected and G.is_directed():
-    _directed_edges(G, X, ax, is_3d, edge_kwargs)
+    _directed_edges(G, X, ax, is_3d, edge_kwargs, cmap)
   else:
-    _undirected_edges(G, X, ax, is_3d, edge_kwargs)
+    _undirected_edges(G, X, ax, is_3d, edge_kwargs, cmap)
   ax.scatter(*X.T, **vertex_kwargs)
   ax.autoscale_view()
   if title:
@@ -84,33 +85,34 @@ def _parse_fmt(fmt, color_key='colors', ls_key='linestyles',
   return result
 
 
-def _directed_edges(G, X, ax, is_3d, edge_style):
+def _directed_edges(G, X, ax, is_3d, edge_style, cmap):
   ii, jj = G.pairs().T
   start, stop = X[ii].T, X[jj].T
   dirs = stop - start
   if is_3d:
     x,y,z = start
     dx,dy,dz = dirs
-    ax.quiver(x, y, z, dx, dy, dz)
+    ax.quiver(x, y, z, dx, dy, dz, cmap=cmap)
   else:
     x,y = start
     dx,dy = dirs
     if hasattr(ax, 'zaxis'):  # Might be on a 3d plot axis.
       z = np.zeros_like(x)
       dz = np.zeros_like(dx)
-      ax.quiver(x, y, z, dx, dy, dz)
+      ax.quiver(x, y, z, dx, dy, dz, cmap=cmap)
     else:
       args = (x, y, dx, dy)
-      ax.quiver(*args, angles='xy', scale_units='xy', scale=1, headwidth=5)
+      ax.quiver(*args, angles='xy', scale_units='xy', scale=1, headwidth=5,
+                cmap=cmap)
 
 
-def _undirected_edges(G, X, ax, is_3d, edge_style):
+def _undirected_edges(G, X, ax, is_3d, edge_style, cmap):
   t = X[G.pairs()]
   if is_3d:
-    edges = Line3DCollection(t, **edge_style)
+    edges = Line3DCollection(t, cmap=cmap, **edge_style)
     ax.add_collection3d(edges)
   else:
-    edges = LineCollection(t, **edge_style)
+    edges = LineCollection(t, cmap=cmap, **edge_style)
     ax.add_collection(edges, autolim=True)
 
 
