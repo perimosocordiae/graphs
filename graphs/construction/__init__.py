9fd8727f9ff96a40fff0962b419e1e3c0722c725
CJ Carey <perimosocordiae@gmail.com>
2015-04-28 15:14:16 -0400

    Adding module docstrings

--- graphs/construction/__init__.py
+++ graphs/construction/__init__.py
@@ -1,3 +1,18 @@
+'''Graph construction algorithms, including:
+
+ - k-nearest and epsilon-close neighbors
+ - b-matching
+ - directed graph construction
+ - Delaunay and Gabriel graphs
+ - Relative Neighborhood graphs
+ - Manifold Spanning graphs
+ - Sparse Regularized graphs
+ - traditional, perturbed, and disjoint Minimum Spanning Trees
+ - random graphs
+
+Each construction function returns a Graph object.
+'''
+
 from b_matching import *
 from directed import *
 from downsample import *
---
+++ graphs/generators/__init__.py
@@ -0,0 +1,8 @@
+'''Dataset generation functions.
+Submodules must be imported directly before use.
+
+mountain_car : the "Mountain Car" toy domain from reinforcement learning
+shapes : various parameterized shapes
+swiss_roll : the "Swiss Roll" toy domain from manifold learning
+trajectories : helper functions for working with trajectory data
+'''
