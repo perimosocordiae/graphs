cf4c2ab2d43aa300e75c95a2204b60b246468728
CJ Carey <perimosocordiae@gmail.com>
2015-04-28 15:12:38 -0400

    Fixing up the test

--- graphs/construction/tests/test_directed.py
+++ graphs/construction/tests/test_directed.py
@@ -3,10 +3,9 @@ matplotlib.use('template')
 
 import numpy as np
 import unittest
-from numpy.testing import assert_array_equal
 
 from graphs.generators.shapes import SCurve
-from graphs.construction import jeff_graph, jeff_prune_edges
+from graphs.construction import directed_graph
 
 
 class TestDirected(unittest.TestCase):
@@ -14,8 +13,8 @@ class TestDirected(unittest.TestCase):
   def test_jeff_graph(self):
     np.random.seed(1234)
     traj = SCurve().trajectories(5, 20)
-    G, X = jeff_graph(traj, k=5, pruning_thresh=0, return_coords=True)
-    P = jeff_graph(traj, k=5, pruning_thresh=0.1)
+    G, X = directed_graph(traj, k=5, pruning_thresh=0, return_coords=True)
+    P = directed_graph(traj, k=5, pruning_thresh=0.1)
     self.assertEqual(X.shape, (100, 3))
     self.assertEqual(G.num_edges(), 500)
     self.assertEqual(P.num_edges(), 419)
