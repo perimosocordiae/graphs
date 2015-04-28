3ea14cb659a86ca8ba8b6aeba6046d6f18798e4b
CJ Carey <perimosocordiae@gmail.com>
2015-04-28 14:18:15 -0400

    Fixing line endings, renaming functions

--- graphs/construction/directed.py
+++ graphs/construction/directed.py
@@ -3,11 +3,11 @@ from sklearn.metrics.pairwise import paired_distances
 from graphs import Graph
 from neighbors import neighbor_graph
 
-__all__ = ['jeff_graph', 'jeff_prune_edges']
+__all__ = ['directed_graph']
 
 
-def jeff_graph(trajectories, k=5, verbose=False, pruning_thresh=0,
-               return_coords=False):
+def directed_graph(trajectories, k=5, verbose=False, pruning_thresh=0,
+                   return_coords=False):
   '''Directed graph construction alg. from Johns & Mahadevan, ICML '07.
   trajectories: list of NxD arrays of ordered states
   '''
@@ -15,16 +15,16 @@ def jeff_graph(trajectories, k=5, verbose=False, pruning_thresh=0,
   G = neighbor_graph(X, k=k)
   if pruning_thresh > 0:
     traj_len = map(len, trajectories)
-    G = jeff_prune_edges(G, X, traj_len, pruning_thresh, verbose=verbose)
+    G = _prune_edges(G, X, traj_len, pruning_thresh, verbose=verbose)
   if return_coords:
     return G, X
   return G
 
 
-def jeff_prune_edges(G, X, traj_lengths, pruning_thresh=0.1, verbose=False):
+def _prune_edges(G, X, traj_lengths, pruning_thresh=0.1, verbose=False):
   '''Prune edges in graph G via cosine distance with trajectory edges.'''
   W = G.matrix(dense=True).copy()
-  degree = G.degree(kind='out', unweighted=True)
+  degree = G.degree(kind='out', weighted=False)
   i = 0
   num_bad = 0
   for n in traj_lengths:
