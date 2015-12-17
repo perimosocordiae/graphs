211b52c8cdc74ab222ab9e161c4d823bf9b50d6e
CJ Carey <perimosocordiae@gmail.com>
2015-12-17 18:07:38 -0500

    Fixing for Python 3

--- graphs/mixins/__init__.py
+++ graphs/mixins/__init__.py
@@ -1,3 +1,5 @@
-from analysis import AnalysisMixin
-from embed import EmbedMixin
-from viz import VizMixin
+from __future__ import absolute_import
+
+from .analysis import AnalysisMixin
+from .embed import EmbedMixin
+from .viz import VizMixin
