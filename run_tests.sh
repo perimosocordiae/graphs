ebf6a208ce066909f80fc91e5c92b393d4702eb3
CJ Carey <perimosocordiae@gmail.com>
2016-01-19 16:33:50 -0500

    Fixing minor issues

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-test_dirs=$(find -type d -name tests | xargs)
+test_dirs=$(find graphs/ -type d -name tests | xargs)
 nosetests --with-cov --cov-report html --cov=graphs/ $test_dirs \
   && coverage report
 
