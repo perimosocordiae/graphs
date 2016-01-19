3cb60bcecbb45adebb37d683d27c7ab2d824584f
CJ Carey <perimosocordiae@gmail.com>
2016-01-19 17:20:01 -0500

    Fixing py3k errors

--- graphs/construction/tests/test_incremental.py
+++ graphs/construction/tests/test_incremental.py
@@ -1,11 +1,11 @@
 import unittest
 import numpy as np
-from itertools import izip_longest
 from numpy.testing import assert_array_almost_equal
 from sklearn.metrics import pairwise_distances
 
 from graphs.construction import neighbor_graph
 from graphs.construction.incremental import incremental_neighbor_graph
+from graphs.mini_six import zip_longest, range
 
 np.set_printoptions(precision=3, suppress=True)
 
@@ -21,21 +21,21 @@ class TestNeighbors(unittest.TestCase):
   def test_k_range(self):
     k_range = range(1, 5)
     incr_gen = incremental_neighbor_graph(self.pts, k=k_range)
-    for k, G in izip_longest(k_range, incr_gen):
+    for k, G in zip_longest(k_range, incr_gen):
       expected = ngraph(self.pts, k=k)
       assert_array_almost_equal(G.matrix(dense=True), expected)
 
     # non-uniform steps
     k_range = [1, 3, 6]
     incr_gen = incremental_neighbor_graph(self.pts, k=k_range)
-    for k, G in izip_longest(k_range, incr_gen):
+    for k, G in zip_longest(k_range, incr_gen):
       expected = ngraph(self.pts, k=k)
       assert_array_almost_equal(G.matrix(dense=True), expected)
 
   def test_eps_range(self):
     eps_range = np.linspace(0.1, 5.5, 5)
     incr_gen = incremental_neighbor_graph(self.pts, epsilon=eps_range)
-    for eps, G in izip_longest(eps_range, incr_gen):
+    for eps, G in zip_longest(eps_range, incr_gen):
       expected = ngraph(self.pts, epsilon=eps)
       assert_array_almost_equal(G.matrix(dense=True), expected)
 
@@ -43,14 +43,14 @@ class TestNeighbors(unittest.TestCase):
     # varied k with fixed epsilon
     k_range = range(1, 5)
     incr_gen = incremental_neighbor_graph(self.pts, k=k_range, epsilon=3.)
-    for k, G in izip_longest(k_range, incr_gen):
+    for k, G in zip_longest(k_range, incr_gen):
       expected = ngraph(self.pts, k=k, epsilon=3.)
       assert_array_almost_equal(G.matrix(dense=True), expected)
 
     # varied eps with fixed k
     eps_range = np.linspace(0.1, 5.5, 5)
     incr_gen = incremental_neighbor_graph(self.pts, k=3, epsilon=eps_range)
-    for eps, G in izip_longest(eps_range, incr_gen):
+    for eps, G in zip_longest(eps_range, incr_gen):
       expected = ngraph(self.pts, k=3, epsilon=eps)
       assert_array_almost_equal(G.matrix(dense=True), expected)
 
@@ -58,7 +58,7 @@ class TestNeighbors(unittest.TestCase):
     dist = pairwise_distances(self.pts, metric='l1')
     k_range = range(1, 5)
     incr_gen = incremental_neighbor_graph(dist, precomputed=True, k=k_range)
-    for k, G in izip_longest(k_range, incr_gen):
+    for k, G in zip_longest(k_range, incr_gen):
       expected = ngraph(dist, precomputed=True, k=k)
       assert_array_almost_equal(G.matrix(dense=True), expected)
 
--- graphs/mini_six.py
+++ graphs/mini_six.py
@@ -1,9 +1,11 @@
 '''Py3k compatibility hacks.'''
 
-__all__ = ['range']
+__all__ = ['range', 'zip_longest']
 
-# If we're on Python 2, use range instead of range
+# If we're on Python 2, use xrange instead of range, etc
 if type(range(1)) is list:
   range = xrange
+  from itertools import izip_longest as zip_longest
 else:
   range = range
+  from itertools import zip_longest
