9a5a041e0a26c1276c753ba717fb2d77db548cee
CJ Carey <perimosocordiae@gmail.com>
2016-02-11 18:39:38 -0500

    Add bench for label spreading

--- benchmarks/benchmarks/mixins.py
+++ benchmarks/benchmarks/mixins.py
@@ -29,15 +29,28 @@ class Labeling(_RandomFormatsBase):
         self.G.spectral_clustering(2)
 
 
+class LabelSpreading(_RandomFormatsBase):
+    params = [['dense', 'coo', 'csr'], ['rbf', 'none', 'binary']]
+    param_names = ['adj_format', 'kernel']
+
+    def setup(self, *args):
+        _RandomFormatsBase.setup(self, *args)
+        np.random.seed(1234)
+        self.y = np.random.randint(5, size=self.n)
+        self.y[np.random.random(self.n) > 0.5] = -1
+
+    def time_spread_labels(self, _, k):
+        self.G.spread_labels(self.y, kernel=k)
+
+
 class Regression(_RandomFormatsBase):
     params = [['dense', 'coo', 'csr'], ['rbf', 'none', 'binary'], [0, 1e-3]]
     param_names = ['adj_format', 'kernel', 'smoothness_penalty']
 
     def setup(self, *args):
         _RandomFormatsBase.setup(self, *args)
-        self.regression_y = np.random.random((self.n//2, 1))
-        self.regression_mask = slice(None, None, 2)
+        self.y = np.random.random((self.n//2, 1))
+        self.mask = slice(None, None, 2)
 
     def time_regression(self, _, k, s):
-        self.G.regression(self.regression_y, self.regression_mask,
-                          smoothness_penalty=s, kernel=k)
+        self.G.regression(self.y, self.mask, smoothness_penalty=s, kernel=k)
