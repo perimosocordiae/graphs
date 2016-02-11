bd593a329bbff65bd22c646b17786367f6be8e1a
CJ Carey <perimosocordiae@gmail.com>
2016-02-11 18:26:48 -0500

    Fixing up the regression benches

--- benchmarks/benchmarks/mixins.py
+++ benchmarks/benchmarks/mixins.py
@@ -5,32 +5,39 @@ import numpy as np
 from graphs import Graph
 
 
-class Labeling(object):
+class _RandomFormatsBase(object):
+    n = 500
+    density = 0.05
     params = ['dense', 'coo', 'csr']
     param_names = ['adj_format']
 
-    def setup(self, adj_format):
-        n = 500
-        density = 0.05
-        adj = ss.rand(n, n, density=density)
+    def setup(self, adj_format, *args):
+        adj = ss.rand(self.n, self.n, density=self.density, random_state=1234)
         if adj_format == 'dense':
             adj = adj.A
         else:
             adj = adj.asformat(adj_format)
         self.G = Graph.from_adj_matrix(adj)
         self.G.symmetrize()
-        self.regression_y = np.random.random((n//2,1))
-        self.regression_mask = slice(None, None, 2)
 
-    def time_greedy_coloring(self, adj_format):
+
+class Labeling(_RandomFormatsBase):
+    def time_greedy_coloring(self, *args):
         self.G.greedy_coloring()
 
-    def time_spectral_clustering(self, adj_format):
+    def time_spectral_clustering(self, *args):
         self.G.spectral_clustering(2)
 
-    def time_regression_no_smooth(self, adj_format):
-        self.G.regression(self.regression_y, self.regression_mask)
 
-    def time_regression_smooth(self, adj_format):
+class Regression(_RandomFormatsBase):
+    params = [['dense', 'coo', 'csr'], ['rbf', 'none', 'binary'], [0, 1e-3]]
+    param_names = ['adj_format', 'kernel', 'smoothness_penalty']
+
+    def setup(self, *args):
+        _RandomFormatsBase.setup(self, *args)
+        self.regression_y = np.random.random((self.n//2, 1))
+        self.regression_mask = slice(None, None, 2)
+
+    def time_regression(self, _, k, s):
         self.G.regression(self.regression_y, self.regression_mask,
-                          smoothness_penalty=1e-3)
+                          smoothness_penalty=s, kernel=k)
