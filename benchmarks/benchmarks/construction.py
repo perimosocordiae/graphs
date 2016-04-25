b8c383a8da1aabed435494baf1073e5645a9a465
CJ Carey <perimosocordiae@gmail.com>
2016-04-25 18:49:44 -0400

    Extending benchmark for neighbor_graph

--- benchmarks/benchmarks/construction.py
+++ benchmarks/benchmarks/construction.py
@@ -3,17 +3,20 @@ from sklearn.metrics import pairwise_distances
 import graphs.construction as gc
 
 
-class KNeighbors(object):
+class Neighbors(object):
     n = 500
-    params = [[1, 10, 100], ['none', 'binary']]
-    param_names = ['k', 'weighting']
+    params = [[None, 0.25, 0.5], [None, 1, 100], ['none', 'binary']]
+    param_names = ['epsilon', 'k', 'weighting']
 
-    def setup(self, k, weighting):
+    def setup(self, epsilon, k, weighting):
+        if epsilon is None and k is None:
+            raise NotImplementedError()
         self.X = np.random.random((self.n, 3))
         self.D = pairwise_distances(self.X)
 
-    def time_knn(self, k, weighting):
-        gc.neighbor_graph(self.X, k=k, weighting=weighting)
+    def time_neighbor_graph(self, epsilon, k, weighting):
+        gc.neighbor_graph(self.X, k=k, epsilon=epsilon, weighting=weighting)
 
-    def time_knn_precomputed(self, k, weighting):
-        gc.neighbor_graph(self.D, k=k, weighting=weighting, precomputed=True)
+    def time_neighbor_graph_precomputed(self, epsilon, k, weighting):
+        gc.neighbor_graph(self.D, k=k, epsilon=epsilon, weighting=weighting,
+                          precomputed=True)
