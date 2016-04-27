e4a7b777bcfb3bec34c2ddf52abfff1742d34a8b
CJ Carey <perimosocordiae@gmail.com>
2016-04-27 17:35:30 -0400

    Refactoring the downsample module
    Adding a FCM-based downsampler as well

--- graphs/construction/downsample.py
+++ graphs/construction/downsample.py
@@ -1,17 +1,26 @@
 from __future__ import absolute_import
 
 import numpy as np
+import warnings
+from sklearn.metrics.pairwise import pairwise_distances
+
+from ..mini_six import range
 from .neighbors import nearest_neighbors
 
-__all__ = ['downsample', 'downsample_trajectories']
+__all__ = [
+    'downsample_trajectories', 'epsilon_net', 'fuzzy_c_means'
+]
 
 
-def downsample_trajectories(trajectories, close_distance):
-  '''Downsamples all the points together,
-     then re-splits into original trajectories.'''
+def downsample_trajectories(trajectories, downsampler, *args, **kwargs):
+  '''Downsamples all points together, then re-splits into original trajectories.
+
+  trajectories : list of 2-d arrays, each representing a trajectory
+  downsampler(X, *args, **kwargs) : callable that returns indices into X
+  '''
   X = np.vstack(trajectories)
   traj_lengths = list(map(len, trajectories))
-  inds = np.sort(downsample(X, close_distance))
+  inds = np.sort(downsampler(X, *args, **kwargs))
   new_traj = []
   for stop in np.cumsum(traj_lengths):
     n = np.searchsorted(inds, stop)
@@ -20,11 +29,11 @@ def downsample_trajectories(trajectories, close_distance):
   return new_traj
 
 
-def downsample(points, close_distance):
+def epsilon_net(points, close_distance):
   '''Selects a subset of `points` to preserve graph structure while minimizing
   the number of points used, by removing points within `close_distance`.
   Returns the downsampled indices.'''
-  num_points, num_dims = points.shape
+  num_points = points.shape[0]
   indices = set(range(num_points))
   selected = []
   while indices:
@@ -33,3 +42,31 @@ def downsample(points, close_distance):
     indices.difference_update(nn_inds)
     selected.append(idx)
   return selected
+
+
+def fuzzy_c_means(points, num_centers, m=2., tol=1e-4, max_iter=100):
+  '''Uses Fuzzy C-Means to downsample `points`.
+  m : aggregation parameter >1, larger implies smoother clusters
+  Returns indices of downsampled points.
+  '''
+  # randomly initialize cluster assignments matrix
+  assn = np.random.random((points.shape[0], num_centers))
+  # iterate assignments until they converge
+  for i in range(max_iter):
+    # compute centers
+    w = assn ** m
+    w /= w.sum(axis=0)
+    centers = w.T.dot(points)
+    # calculate new assignments
+    d = pairwise_distances(points, centers)
+    d **= 2. / (m - 1)
+    new_assn = 1. / np.einsum('ik,ij->ik', d, 1./d)
+    # check for convergence
+    change = np.linalg.norm(new_assn - assn)
+    if change < tol:
+      break
+    assn = new_assn
+  else:
+    warnings.warn("fuzzy_c_means didn't converge in %d iterations" % max_iter)
+  # find points closest to the selected cluster centers
+  return d.argmin(axis=0)
--- graphs/construction/tests/test_downsample.py
+++ graphs/construction/tests/test_downsample.py
@@ -3,25 +3,30 @@ import unittest
 from numpy.testing import assert_array_equal
 
 from graphs.generators.shapes import SCurve
-from graphs.construction import downsample, downsample_trajectories
+from graphs.construction import downsample as d
 
 
 class TestDownsample(unittest.TestCase):
 
-  def test_downsample(self):
+  def test_epsilon_net(self):
     pts = np.array([[0,0],[1,2],[3,2],[-1,0]])
-    sample = downsample(pts, 1.7)
+    sample = d.epsilon_net(pts, 1.7)
     self.assertTupleEqual(tuple(sample), (0,1,2))
     traj = [pts[:2], pts[2:]]
-    sample = downsample_trajectories(traj, 1.7)
+    sample = d.downsample_trajectories(traj, d.epsilon_net, 1.7)
     assert_array_equal(sample[0], pts[:2])
     assert_array_equal(sample[1], pts[2:3])
+
+  def test_fuzzy_c_means(self):
+    pts = np.array([[0,0],[1,2],[3,2],[-1,0]])
+    sample = np.sort(d.fuzzy_c_means(pts, 2))
+    assert_array_equal(sample, [0, 2])
 
   def test_downsample_trajectories(self):
     traj = SCurve().trajectories(5, 20)
     pts = np.vstack(traj)
-    ds_traj = downsample_trajectories(traj, 0.05)
-    ds_pts = pts[downsample(pts, 0.05)]
+    ds_traj = d.downsample_trajectories(traj, d.epsilon_net, 0.05)
+    ds_pts = pts[d.epsilon_net(pts, 0.05)]
     assert_array_equal(np.vstack(ds_traj), ds_pts)
 
 
