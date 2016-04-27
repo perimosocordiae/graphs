e91be9df74ab27aa56a6aa578563bd0e5c004131
CJ Carey <perimosocordiae@gmail.com>
2016-04-27 19:57:20 -0400

    Adding verbose output, preventing NaN errors

--- graphs/construction/downsample.py
+++ graphs/construction/downsample.py
@@ -44,11 +44,15 @@ def epsilon_net(points, close_distance):
   return selected
 
 
-def fuzzy_c_means(points, num_centers, m=2., tol=1e-4, max_iter=100):
+def fuzzy_c_means(points, num_centers, m=2., tol=1e-4, max_iter=100,
+                  verbose=False):
   '''Uses Fuzzy C-Means to downsample `points`.
   m : aggregation parameter >1, larger implies smoother clusters
   Returns indices of downsampled points.
   '''
+  num_points = points.shape[0]
+  if num_centers >= num_points:
+    return np.arange(num_points)
   # randomly initialize cluster assignments matrix
   assn = np.random.random((points.shape[0], num_centers))
   # iterate assignments until they converge
@@ -60,9 +64,12 @@ def fuzzy_c_means(points, num_centers, m=2., tol=1e-4, max_iter=100):
     # calculate new assignments
     d = pairwise_distances(points, centers)
     d **= 2. / (m - 1)
+    np.maximum(d, 1e-10, out=d)
     new_assn = 1. / np.einsum('ik,ij->ik', d, 1./d)
     # check for convergence
     change = np.linalg.norm(new_assn - assn)
+    if verbose:
+      print('At iteration %d: change = %g' % (i+1, change))
     if change < tol:
       break
     assn = new_assn
