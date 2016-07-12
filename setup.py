f0612393faf3c1e8cd73b7b0c508d40c78edac5a
CJ Carey <perimosocordiae@gmail.com>
2016-07-12 13:43:34 -0400

    Version bump: v0.0.4
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python
+from setuptools import setup, find_packages, Extension
 
-from setuptools import setup, find_packages
+try:
+  from Cython.Build import cythonize
+  import numpy as np
+except ImportError:
+  use_cython = False
+else:
+  use_cython = True
 
-setup(
+setup_kwargs = dict(
     name='graphs',
-    version='0.0.3',
+    version='0.0.4',
     author='CJ Carey',
     author_email='perimosocordiae@gmail.com',
-    description='All things graph.',
+    description='A library for graph-based machine learning.',
     url='http://github.com/all-umass/graphs',
     license='MIT',
     packages=find_packages(exclude=['tests']),
+    package_data={'': ['*.pyx']},
     install_requires=[
         'numpy >= 1.8',
         'scipy >= 0.14',
         'scikit-learn >= 0.15',
         'matplotlib >= 1.3.1',
+        'Cython >= 0.21',
     ],
 )
+if use_cython:
+  exts = [Extension('*', ['graphs/*/*.pyx'], include_dirs=[np.get_include()])]
+  setup_kwargs['ext_modules'] = cythonize(exts)
+
+setup(**setup_kwargs)
