84e46bfb1d1ce3e66582a38217645063515ada87
CJ Carey <perimosocordiae@gmail.com>
2016-07-20 14:39:32 -0400

    Adding chain/lattice graph generators

--- graphs/generators/__init__.py
+++ graphs/generators/__init__.py
@@ -5,3 +5,4 @@ trajectories : helpers for working with trajectory data
 from __future__ import absolute_import
 
 from . import trajectories
+from .structured import chain_graph, lattice_graph
---
+++ graphs/generators/structured.py
@@ -0,0 +1,49 @@
+from __future__ import absolute_import
+import numpy as np
+import scipy.sparse as ss
+
+from .. import Graph
+
+
+def chain_graph(num_vertices, wraparound=False, directed=False, weights=None):
+  if wraparound:
+    ii = np.arange(num_vertices)
+    jj = ii + 1
+    jj[-1] = 0
+  else:
+    ii = np.arange(num_vertices-1)
+    jj = ii + 1
+  pairs = np.column_stack((ii, jj))
+  return Graph.from_edge_pairs(pairs, num_vertices=num_vertices,
+                               symmetric=(not directed), weights=weights)
+
+
+def lattice_graph(dims, wraparound=False):
+  if not dims or len(dims) == 0:
+    raise ValueError('Must supply at least 1 dimension')
+  if len(dims) == 1:
+    return chain_graph(dims[0], wraparound=wraparound)
+  if len(dims) > 2:
+    raise NotImplementedError('NYI: len(dims) > 2')
+
+  # 2d case
+  m, n = dims
+  num_vertices = m * n
+  if wraparound:
+    offsets = [-m*(n-1), -m, -m+1, -1, 1, m-1, m, m*(n-1)]
+    data = np.ones((8, num_vertices), dtype=int)
+    data[[2,5], :] = 0
+    data[2, ::m] = 1
+    data[3, m-1::m] = 0
+    data[4, ::m] = 0
+    data[5, m-1::m] = 1
+    # handle edge cases where m-1 == 1, etc
+    offsets, idx = np.unique(offsets, return_index=True)
+    data = data[idx]
+  else:
+    offsets = [-m, -1, 1, m]
+    data = np.ones((4, num_vertices), dtype=int)
+    data[1, m-1::m] = 0
+    data[2, 0::m] = 0
+  adj = ss.dia_matrix((data, offsets), shape=(num_vertices, num_vertices))
+  return Graph.from_adj_matrix(adj)
